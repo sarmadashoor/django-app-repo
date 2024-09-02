361dec63440a281026604d442ac6042b2c871c7a
sashoor <sarmadashoor1@gmail.com>
2024-09-02 13:35:30 -0700

    adding new changes

--- config/settings.py
+++ config/settings.py
@@ -8,12 +8,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') -eq 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1 [::1]').Split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1 [::1]').split()
 
 # Application definition
-INSTALLED_APPS = @(
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -22,9 +22,9 @@ INSTALLED_APPS = @(
     'django.contrib.staticfiles',
     'rest_framework',
     'apps.example_app'
-)
+]
 
-MIDDLEWARE = @(
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -32,46 +32,46 @@ MIDDLEWARE = @(
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
-)
+]
 
 ROOT_URLCONF = 'config.urls'
 
-TEMPLATES = @(
-    @{
-        'BACKEND' = 'django.template.backends.django.DjangoTemplates'
-        'DIRS' = @("/templates")
-        'APP_DIRS' = True
-        'OPTIONS' = @{
-            'context_processors' = @(
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages'
-            )
-        }
-    }
-)
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
-DATABASES = @{
-    'default' = @{
-        'ENGINE' = 'django.db.backends.postgresql'
-        'NAME' = (os.getenv('POSTGRES_DB'))
-        'USER' = (os.getenv('POSTGRES_USER'))
-        'PASSWORD' = (os.getenv('POSTGRES_PASSWORD'))
-        'HOST' = (os.getenv('POSTGRES_HOST'))
-        'PORT' = (os.getenv('POSTGRES_PORT'))
-    }
-    'mongo' = @{
-        'ENGINE' = 'djongo'
-        'NAME' = 'my_mongo_db'
-        'ENFORCE_SCHEMA' = False
-        'CLIENT' = @{
-            'host' = (os.getenv('MONGO_HOST'))
-            'port' = [int](os.getenv('MONGO_PORT'))
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    },
+    'mongo': {
+        'ENGINE': 'djongo',
+        'NAME': 'my_mongo_db',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': os.getenv('MONGO_HOST'),
+            'port': int(os.getenv('MONGO_PORT')),
+        },
     }
 }
 
